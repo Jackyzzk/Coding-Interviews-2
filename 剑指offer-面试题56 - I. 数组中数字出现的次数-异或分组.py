2dcb0e28a1818010b2e3783d7fe5ec9782fd49cc
Jackyzzk <417759979@qq.com>
2020-04-19 23:32:08 +0800

    continue

---
+++ 剑指offer-面试题56 - I. 数组中数字出现的次数-异或分组.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    """
+一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。
+请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+输入：nums = [4,1,4,6]
+输出：[1,6] 或 [6,1]
+输入：nums = [1,2,10,4,1,4,3,3]
+输出：[2,10] 或 [10,2]
+2 <= nums <= 10000
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof
+    """
+    def singleNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+
+def main():
+    nums = [1, 2, 10, 4, 1, 4, 3, 3]
+    test = Solution()
+    ret = test.singleNumbers(nums)
+    print(ret)
+
+
+if __name__ == '__main__':
+    main()
